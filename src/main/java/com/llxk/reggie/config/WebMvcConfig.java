2a43ef3edc24a9ef15558f7aab4e6fbd2c8ea18e
llxk <1820472818@qq.com>
2023-04-15 00:02:41 +0800

    准备工作完成

---
+++ src/main/java/com/llxk/reggie/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.llxk.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * ClassName: WebMvcConfig
+ * Package: com.llxk.reggie.config
+ *
+ * @author 庐陵小康
+ * @version 1.0
+ * @Desc
+ * @Date 2023/4/14 23:40
+ */
+@Configuration
+@Slf4j
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行静态资源映射...");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
